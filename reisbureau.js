8ca0f308329fdba1e05cf89257ac2561b6662cf0
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-13 14:49:14 +0200

    Made changes to the array and code to be able to sort it on both price and place. Next step is creating a loop to output the entire list in that order

--- reisbureau.js
+++ reisbureau.js
@@ -1,5 +1,43 @@
-function showHol(){
+allHol = [ {
+    place: "Denmark",
+    price: 340
+},
+{
+    place: "England",
+    price: 250
+},
+{
+    place: "Egypt",
+    price: 560
+},
+{
+    place: "China",
+    price: 890
+},
+{
+    place: "India",
+    price: 1340
+},
+{
+    place: "Japan",
+    price: 1250
+}
+]
+
+var sortPrice = allHol.sort(function(a, b){
+ return a.price - b.price;
+});
+
+function ssPrice(){
+    document.getElementById("listHol").innerHTML = sortPrice[0].place +" "+ sortPrice[0].price;
+}
+
+console.log(sortPrice)
+
+/*
 var allHol = [["Australia", "1350"], ["Germany", "200"], ["UK", "250"], ["Italy", "400"], ["America", "1250"]];
+function showHol(){
+
 var colHol = 1;
 htmlTable = "<table><tr>";
 
@@ -16,4 +54,8 @@ htmlTable+= "</tr></table>";
 document.getElementById("listHol").innerHTML = htmlTable;
 };
 
+function sortHol(){
+    allHol.sort(a,b);
+}
 
+*/
