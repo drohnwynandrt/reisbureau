a188d01f4501303d9af2f4899974f3b32f8f7596
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-13 12:20:20 +0200

    Made a JS that automatically creates a table based on the array

--- reisbureau.js
+++ reisbureau.js
@@ -1,2 +1,19 @@
-var allholidays = [["Australia", "1350"], ["Germany", "200"], ["UK", "250"], ["Italy", "400"], ["America", "1250"]];
+function showHol(){
+var allHol = [["Australia", "1350"], ["Germany", "200"], ["UK", "250"], ["Italy", "400"], ["America", "1250"]];
+var colHol = 1;
+htmlTable = "<table><tr>";
+
+for (var i=0; i<allHol.length;i++){
+    htmlTable += "<td>" + allHol[i][0] +"</td><td>" +allHol[i][1] + "</td>";
+    var next=i+1;
+    if(next%colHol==0 && next!=allHol.length){
+        htmlTable += "</tr><tr>";
+    
+    }
+}
+htmlTable+= "</tr></table>";
+
+document.getElementById("listHol").innerHTML = htmlTable;
+};
+
 
